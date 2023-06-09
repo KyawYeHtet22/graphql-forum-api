13ccdece68d3c3ed5c014c2ec3e25ebe57596b9b
Kyaw Ye Htet <kyawyehtet@amdon.com>
2023-06-09 16:29:00 +0630

    create graphql server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+
+const app = express()
+const port = 4000
+
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`
+const resolvers = {
+  Query: {
+    sayHello: () => 'Hello GraphQL!'
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.applyMiddleware({ app, cors: true })
+
+app.listen({ port }, () => {
+  console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`)
+})
